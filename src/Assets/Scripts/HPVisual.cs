65bcaf030f585755c49ad439f388746622294ec4
knarferuom <mourefrank@gmail.com>
2026-04-01 14:18:59 -0400

    boinjg

--- src/Assets/Scripts/HPVisual.cs
+++ src/Assets/Scripts/HPVisual.cs
@@ -1,29 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class HPVisual : MonoBehaviour
 {
     public int id;
-    int before;
-    int after;
+    private int before;
+    private int after;
+    public GameObject gameManagerObject;
     private Animator animator;
+    private GameManager manager;
 
     void Start()
     {
-        GameManager manager = GameObject.Find("GameManager").GetComponent<GameManager>();
-
         animator = GetComponent<Animator>();
-        before = manager.ply1.TotalHealth(manager.BlackjackThreshold, false);
+        animator.enabled = false;
+        StartCoroutine(LateStart());
     }
-    public void flipOrNot()
+
+    IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(.1f);
+        manager = gameManagerObject.GetComponent<GameManager>();
+        before = manager.ply1.TotalHealth(manager.BlackjackThreshold, false);
+        after = before;
+    }
+
+    public IEnumerator flipOrNot()
     {
         if (after >= id){
             if (before < id){
                 //flip to show gain
+                animator.SetBool("playerHP", true);
+                yield return new WaitForSeconds(.5f);
+                animator.SetBool("YouSide", true);
             }
         }
         if (after <= id){
             if (before > id){
                 //flip to show loss
+                yield return new WaitForSeconds(.5f);
             }
         }
     }
