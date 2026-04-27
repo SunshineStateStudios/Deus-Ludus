d5460aca90e42313cdbdf0e36c5e2aca93663175
plex <aksiimovg@tutamail.com>
2026-04-27 13:57:39 -0400

    Summary (required)

--- src/Assets/Scripts/MainMenuHandler.cs
+++ src/Assets/Scripts/MainMenuHandler.cs
@@ -3,8 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
+    private string CurrentMenu = "Main";
+
+    public void GoToSettings() {
+        
+    }
+
     public void Play()
     {
+        if (!CurrentMenu.Equals("Main")) return;
         LoadScene(1);
     }
     public void LoadScene(int sceneIndex)
