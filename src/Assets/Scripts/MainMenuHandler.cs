c9314953830d57e77daa0759521257f145a093ba
knarferuom <mourefrank@gmail.com>
2026-04-29 13:14:17 -0400

    y

--- src/Assets/Scripts/MainMenuHandler.cs
+++ src/Assets/Scripts/MainMenuHandler.cs
@@ -6,12 +6,20 @@ public class MainMenuHandler : MonoBehaviour
     private string CurrentMenu = "Main";
 
     public void GoToSettings() {
-        
+        Debug.Log("settings");
     }
 
     public void Play()
     {
         if (!CurrentMenu.Equals("Main")) return;
         SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+    public void Credits()
+    {
+        Debug.Log("Credits");
     }
 }
