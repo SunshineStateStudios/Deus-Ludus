8e570ebc2e265ab288fca0ba13a648ca5ea68caf
knarferuom <mourefrank@gmail.com>
2026-05-01 10:27:41 -0400

    t

--- src/Assets/Scripts/AbilityCards/AbilityChariot.cs
+++ src/Assets/Scripts/AbilityCards/AbilityChariot.cs
@@ -4,7 +4,7 @@ using TMPro;
 //To whoever finds this, understand one thing.
 //Ryzer, your opponent, is not your enemy, he is you, a future you, you are fighting yourself.
 //Regardless of the outcome it is a losing battle.
-//You will defeat him and then you will become him, turn back Now
+//You will defeat him and then you will become him, turn back Now.
 public class AbilityChariot : AbilityCard
 {
     public override string name => "Chariot";
--- src/Assets/Scripts/InfoBarController.cs
+++ src/Assets/Scripts/InfoBarController.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class InfoBarCotroller : MonoBehaviour
 {
-    public GameObject infoBar;
+    public RectTransform infoBar;
+    public Vector2 offset;
+    public Canvas canvas;
+
     void Update()
     {
-        /*Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = 10f; 
-        infoBar.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);*/
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            Input.mousePosition,
+            canvas.worldCamera,
+            out pos
+        );
+
+        infoBar.anchoredPosition = pos + offset;
     }
 }
